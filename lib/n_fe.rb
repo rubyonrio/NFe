cb3eee5d633aff7ec0025bf23314aed3ca1ad6ab
Vagner Zampieri <vgzampieri@gmail.com>
2012-02-03 15:43:45 -0200

    Add setup

--- lib/n_fe.rb
+++ lib/n_fe.rb
@@ -5,6 +5,10 @@ module NFe
   mattr_accessor :provider_tax_number
   @@provider_tax_number = nil
 
+  # method for taking CCM
+  mattr_accessor :provider_taxpayer_register
+  @@provider_taxpayer_register = nil
+
   def self.setup
     yield self
   end
